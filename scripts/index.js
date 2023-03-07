1f6aaf72077d3931f7c0ca748ce3658387ad8fbe
AlexIT <alexit81@yandex.ru>
2023-03-07 17:47:18 +0300

    feat: реализовать добавление карточек из массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
-  let triggerModalEdit = document.querySelector(".profile__edit-btn"),
+  const initialCards = [
+    {name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'},
+    {name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'},
+    {name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'},
+    {name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
+    {name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'},
+    {name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
+  ];
+
+  const cardsPlace = document.querySelector(".elements"),
+    triggerModalEdit = document.querySelector(".profile__edit-btn"),
     modalEdit = document.querySelector(".popup"),
     closeModals = document.querySelector(".popup__close"),
     nameValue = document.querySelector(".profile__title"),
@@ -28,4 +38,26 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   formElement.addEventListener("submit", handleFormSubmit);
+
+  // Добавление карточек
+
+  initialCards.forEach((item) => {
+    AddCards(item.name, item.link);
+  });
+
+  function AddCards(nameCard, linkCard) {
+    const cardsTemlate = document.querySelector('#element').content;
+    const cardElement = cardsTemlate.querySelector('.element').cloneNode(true);
+
+    cardElement.querySelector('.element__img').src = linkCard;
+    cardElement.querySelector('.element__img').alt = nameCard;
+    cardElement.querySelector('.element__title').textContent = nameCard;
+    cardsPlace.appendChild(cardElement);
+  }
+
+  AddCards('Архыз', 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg');
+
+
+
+
 });
