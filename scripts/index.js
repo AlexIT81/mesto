337827eed34ece9dbcdd20278c2764af8770e6b6
AlexIT <alexit81@yandex.ru>
2023-03-09 14:19:36 +0300

    feat: сверстать модалку с картинкой

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,29 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const initialCards = [
-    {name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'},
-    {name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'},
-    {name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'},
-    {name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'},
-    {name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'},
-    {name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'}
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    },
   ];
 
   const cardsPlace = document.querySelector(".elements"),
@@ -13,8 +31,10 @@ window.addEventListener('DOMContentLoaded', () => {
     triggerModalAdd = document.querySelector(".profile__add-btn"),
     modalEdit = document.querySelector(".popup_edit"),
     modalAdd = document.querySelector(".popup_add"),
+    modalImage = document.querySelector(".popup_image"),
     closeBtnModalEdit = modalEdit.querySelector(".popup__close"),
     closeBtnModalAdd = modalAdd.querySelector(".popup__close"),
+    closeBtnModalImage = modalImage.querySelector(".popup__close"),
     nameValue = document.querySelector(".profile__title"),
     jobValue = document.querySelector(".profile__sub-title"),
     nameInput = modalEdit.querySelectorAll(".popup__input")[0],
@@ -29,12 +49,12 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   function AddCards(nameCard, linkCard) {
-    const cardsTemlate = document.querySelector('#element').content,
-      cardElement = cardsTemlate.querySelector('.element').cloneNode(true);
+    const cardsTemlate = document.querySelector("#element").content,
+      cardElement = cardsTemlate.querySelector(".element").cloneNode(true);
 
-    cardElement.querySelector('.element__img').src = linkCard;
-    cardElement.querySelector('.element__img').alt = nameCard;
-    cardElement.querySelector('.element__title').textContent = nameCard;
+    cardElement.querySelector(".element__img").src = linkCard;
+    cardElement.querySelector(".element__img").alt = nameCard;
+    cardElement.querySelector(".element__title").textContent = nameCard;
     cardsPlace.prepend(cardElement);
   }
 
@@ -63,10 +83,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   formElementEdit.addEventListener("submit", handleFormEditSubmit);
 
-
   // Окно добавления карточки + добавление карточки
 
-  triggerModalAdd.addEventListener('click', () => {
+  triggerModalAdd.addEventListener("click", () => {
     modalAdd.classList.add("popup_opened");
   });
 
@@ -77,10 +96,10 @@ window.addEventListener('DOMContentLoaded', () => {
   function handleFormAddSubmit(evt) {
     evt.preventDefault();
     let titleInput = modalAdd.querySelectorAll(".popup__input")[0],
-    linkInput = modalAdd.querySelectorAll(".popup__input")[1];
+      linkInput = modalAdd.querySelectorAll(".popup__input")[1];
     AddCards(titleInput.value, linkInput.value);
-    titleInput.value = '';
-    linkInput.value = '';
+    titleInput.value = "";
+    linkInput.value = "";
     closeModal(modalAdd);
   }
 
@@ -92,20 +111,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
   Array.from(elementsIcon).forEach((element) => {
     element.addEventListener("click", () => {
-      element.classList.toggle('element__icon_active');
+      element.classList.toggle("element__icon_active");
     });
   });
 
   //Удаление карточки
 
-  const trashBtns = document.querySelectorAll('.element__trash ');
+  const trashBtns = document.querySelectorAll(".element__trash");
 
   trashBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      // let removeCard = btn.closest('.element');
-      // removeCard.remove();
-      btn.closest('.element').remove();
+    btn.addEventListener("click", () => {
+      btn.closest(".element").remove();
     });
   });
+
+  // Открытие модального окна с картинкой
+
+
 
 });
