f3fc96d5ab73cfb22d9a9462ea8000a2f590c573
AlexIT <alexit81@yandex.ru>
2023-04-16 15:52:18 +0300

    feat: реализовать class Card

---
+++ scripts/Card.js
@@ -0,0 +1,46 @@
+import { createModalImage, openModal } from "./index.js";
+
+class Card {
+  constructor(data,templateSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._templateSelector = templateSelector;
+  }
+
+  /** Находим template */
+  _getTemplate() {
+    const cardElement = document.querySelector(this._templateSelector).content.querySelector(".element").cloneNode(true);
+    return cardElement;
+  }
+
+  /** Навешиваем обработчики */
+  _setEventListeners() {
+    this._element = this._getTemplate();
+    const cardElementTrash =this._element.querySelector(".element__trash"),
+      cardElementIcon = this._element.querySelector(".element__icon"),
+      cardElementImg = this._element.querySelector(".element__img");
+    cardElementTrash.addEventListener("click", (e) => {
+      e.target.closest(".element").remove()
+    });
+    cardElementIcon.addEventListener("click", (e) =>
+    e.target.classList.toggle("element__icon_active")
+    );
+    cardElementImg.addEventListener("click", (e) => {
+      createModalImage(e);
+      openModal(document.querySelector(".popup_image"));
+    });
+  }
+
+  /** Функция создания карточки */
+  createCard() {
+    this._setEventListeners();
+    const cardElementTitle = this._element.querySelector(".element__title"),
+    cardElementImg = this._element.querySelector(".element__img");
+    cardElementImg.src = this._link;
+    cardElementImg.alt = this._name;
+    cardElementTitle.textContent = this._name;
+    return this._element;
+  }
+}
+
+export {Card};
--- scripts/index.js
+++ scripts/index.js
@@ -1,125 +1,109 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const cardWrapper = document.querySelector(".elements"),
-    triggerModalEdit = document.querySelector(".profile__edit-btn"),
-    triggerModalAdd = document.querySelector(".profile__add-btn"),
-    modalEdit = document.querySelector(".popup_edit"),
-    modalAdd = document.querySelector(".popup_add"),
-    modalImage = document.querySelector(".popup_image"),
-    nameValue = document.querySelector(".profile__title"),
-    jobValue = document.querySelector(".profile__sub-title"),
-    nameInput = modalEdit.querySelector(".popup__input_name"),
-    jobInput = modalEdit.querySelector(".popup__input_job"),
-    formElementEdit = document.forms["edit"],
-    formElementAdd = document.forms["add"],
-    modalImageFigure = modalImage.querySelector(".popup__big-image"),
-    modalImageFigcaption = modalImage.querySelector(".popup__figcaption"),
-    cardTemlate = document.querySelector("#element").content,
-    titleInput = modalAdd.querySelector(".popup__input_title"),
-    linkInput = modalAdd.querySelector(".popup__input_link"),
-    modalWindows = document.querySelectorAll(".popup");
+import { Card } from './Card.js';
 
-  /** Функция открытия модального окна */
-  function openModal(modal) {
-    modal.classList.add("popup_opened");
-    document.addEventListener("keydown", closeModalByPressEsc);
+const cardWrapper = document.querySelector(".elements"),
+  triggerModalEdit = document.querySelector(".profile__edit-btn"),
+  triggerModalAdd = document.querySelector(".profile__add-btn"),
+  modalEdit = document.querySelector(".popup_edit"),
+  modalAdd = document.querySelector(".popup_add"),
+  nameValue = document.querySelector(".profile__title"),
+  jobValue = document.querySelector(".profile__sub-title"),
+  nameInput = modalEdit.querySelector(".popup__input_name"),
+  jobInput = modalEdit.querySelector(".popup__input_job"),
+  formElementEdit = document.forms["edit"],
+  formElementAdd = document.forms["add"],
+  modalImage = document.querySelector(".popup_image"),
+  modalImageFigure = modalImage.querySelector(".popup__big-image"),
+  modalImageFigcaption = modalImage.querySelector(".popup__figcaption"),
+  titleInput = modalAdd.querySelector(".popup__input_title"),
+  linkInput = modalAdd.querySelector(".popup__input_link"),
+  modalWindows = document.querySelectorAll(".popup");
+
+/** Функция открытия модального окна */
+function openModal(modal) {
+  modal.classList.add("popup_opened");
+  document.addEventListener("keydown", closeModalByPressEsc);
+}
+
+/** Функция-обработчик закрытия модалки при нажатии Esc */
+function closeModalByPressEsc(e) {
+  if (e.key === "Escape") {
+    const openedModal = document.querySelector(".popup_opened");
+    closeModal(openedModal);
   }
+}
 
-  /** Функция-обработчик закрытия модалки при нажатии Esc */
-  function closeModalByPressEsc(e) {
-    if (e.key === "Escape") {
-      const openedModal = document.querySelector(".popup_opened");
-      closeModal(openedModal);
-    }
+/** Функция закрытия модального окна */
+function closeModal(modal) {
+  document.removeEventListener("keydown", closeModalByPressEsc);
+  modal.classList.remove("popup_opened");
+  if (modal.classList.contains('popup_image')) {
+    modalImageFigure.src = '';
+    modalImageFigure.alt = '';
+    modalImageFigcaption.textContent = '';
   }
+}
 
-  /** Функция закрытия модального окна */
-  function closeModal(modal) {
-    document.removeEventListener("keydown", closeModalByPressEsc);
-    modal.classList.remove("popup_opened");
-  }
-
-  /** Закрытие модального окна кликом на оверлей и на крестик */
-  modalWindows.forEach((modal) => {
-    modal.addEventListener("mousedown", (e) => {
-      if (e.target.classList.contains("popup_opened")) {
-        closeModal(modal);
-      }
-      if (e.target.classList.contains("popup__close")) {
-        closeModal(modal);
-      }
-    });
+/** Закрытие модального окна кликом на оверлей и на крестик */
+modalWindows.forEach((modal) => {
+  modal.addEventListener("mousedown", (e) => {
+    if (e.target.classList.contains("popup_opened") || e.target.classList.contains("popup__close")) closeModal(modal)
   });
-
-  /** Функция создания карточки */
-  function createCard(cardName, cardLink) {
-    const cardElement = cardTemlate.querySelector(".element").cloneNode(true),
-      cardElementImg = cardElement.querySelector(".element__img"),
-      cardElementTitle = cardElement.querySelector(".element__title"),
-      cardElementTrash = cardElement.querySelector(".element__trash"),
-      cardElementIcon = cardElement.querySelector(".element__icon");
-    cardElementImg.src = cardLink;
-    cardElementImg.alt = cardName;
-    cardElementTitle.textContent = cardName;
-    cardElementTrash.addEventListener("click", (e) =>
-      e.target.closest(".element").remove()
-    );
-    cardElementImg.addEventListener("click", (e) => {
-      createModalImage(e);
-      openModal(modalImage);
-    });
-    cardElementIcon.addEventListener("click", (e) =>
-      e.target.classList.toggle("element__icon_active")
-    );
-    return cardElement;
-  }
-
-  /** Функция добавления карточки в DOM */
-  function addCard(card) {
-    cardWrapper.prepend(card);
-  }
-
-  /** Функция генерации модального окна с картинкой из карточки */
-  function createModalImage(e) {
-    const imgUrl = e.target.src,
-      imgDescription = e.target.alt;
-
-    modalImageFigure.src = imgUrl;
-    modalImageFigure.alt = imgDescription;
-    modalImageFigcaption.textContent = imgDescription;
-  }
-
-  /** Функция обработки данных путешественника */
-  function handleFormEditSubmit(e) {
-    e.preventDefault();
-    nameValue.textContent = nameInput.value;
-    jobValue.textContent = jobInput.value;
-    closeModal(modalEdit);
-  }
-
-  /** Функция добавления карточки */
-  function handleFormAddSubmit(e) {
-    e.preventDefault();
-    addCard(createCard(titleInput.value, linkInput.value));
-    e.target.reset();
-    const submitBtn = e.target.querySelector(".popup__button");
-    submitBtn.classList.add("popup__button_disabled");
-    submitBtn.setAttribute("disabled", true);
-    closeModal(modalAdd);
-  }
-
-  /** Начальное добавление карточек в галерею из массива */
-  initialCards.forEach((item) => addCard(createCard(item.name, item.link)));
-
-  /** Редактирование информации о путешественнике */
-  triggerModalEdit.addEventListener("click", () => {
-    openModal(modalEdit);
-    nameInput.value = nameValue.textContent;
-    jobInput.value = jobValue.textContent;
-  });
-
-  formElementEdit.addEventListener("submit", handleFormEditSubmit);
-
-  /** Добавление карточки */
-  triggerModalAdd.addEventListener("click", () => openModal(modalAdd));
-  formElementAdd.addEventListener("submit", handleFormAddSubmit);
 });
+
+/** Функция генерации модального окна с картинкой из карточки */
+function createModalImage(e) {
+  const imgUrl = e.target.src,
+    imgDescription = e.target.alt;
+
+  modalImageFigure.src = imgUrl;
+  modalImageFigure.alt = imgDescription;
+  modalImageFigcaption.textContent = imgDescription;
+}
+
+/** Функция обработки данных путешественника */
+function handleFormEditSubmit(e) {
+  e.preventDefault();
+  nameValue.textContent = nameInput.value;
+  jobValue.textContent = jobInput.value;
+  closeModal(modalEdit);
+}
+
+/** Функция добавления карточки из модального окна */
+function handleFormAddSubmit(e) {
+  e.preventDefault();
+  const cardData = {
+    name: titleInput.value,
+    link: linkInput.value
+  }
+  addCard(new Card(cardData, '#element').createCard());
+  e.target.reset();
+  const submitBtn = e.target.querySelector(".popup__button");
+  submitBtn.classList.add("popup__button_disabled");
+  submitBtn.setAttribute("disabled", true);
+  closeModal(modalAdd);
+}
+
+/** Функция добавления карточки в DOM */
+function addCard(card) {
+  cardWrapper.prepend(card);
+}
+
+/** Начальное добавление карточек в галерею из массива */
+initialCards.forEach((item) => {
+  addCard(new Card(item, '#element').createCard());
+});
+
+/** Редактирование информации о путешественнике */
+triggerModalEdit.addEventListener("click", () => {
+  openModal(modalEdit);
+  nameInput.value = nameValue.textContent;
+  jobInput.value = jobValue.textContent;
+});
+
+formElementEdit.addEventListener("submit", handleFormEditSubmit);
+
+/** Добавление карточки */
+triggerModalAdd.addEventListener("click", () => openModal(modalAdd));
+formElementAdd.addEventListener("submit", handleFormAddSubmit);
+
+export { createModalImage, openModal };
