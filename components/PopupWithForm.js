514f87d13aedcc282c63de2ecfc93ddd52e89b08
AlexIT <alexit81@yandex.ru>
2023-04-29 17:14:22 +0300

    feat: Создать класс PopupWithForm

---
+++ components/PopupWithForm.js
@@ -0,0 +1,27 @@
+import Popup from "./Popup";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('.popup__form');
+
+  }
+
+  _getInputValues(e) {
+    this._inputValues = Object.fromEntries(new FormData(this._form));
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    })
+  }
+
+  close() {
+    this._form.reset();
+    super.close();
+  }
+}
